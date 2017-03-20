30b0d630fd64a752fa0ff04f6d09488e0e776626
Dimpy Chhabra <Dimpy Chhabra>
2017-03-20 22:58:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dimpychhabra/capo/Utils.java
@@ -0,0 +1,25 @@
+package com.example.dimpychhabra.capo;
+
+/**
+ * Created by Dimpy Chhabra on 3/20/2017.
+ */
+
+public class Utils {
+
+    public static final String regEx = "\\b[A-Za-z0-9._%+-] + @[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}\\b";
+
+    //Fragments Tags
+    public static final String Login_Fragment = "Login_Fragment";
+    public static final String SignUp_Fragment = "SignUp_Fragment";
+    public static final String ForgotPassword_Fragment = "ForgotPassword_Fragment";
+
+    //Shared Pref Column Names
+    public static final String MyPref = "MPref";
+    public static final String Name   = "nameKey";
+    public static final String Phone  ="phoneKey";
+    public static final String IS_LOGIN = "is_loged";
+    public static final String displaypic = "picKey";
+    public static final String College = "collegeKey";
+    public static final String Email = "emailKey";
+    public static final String Extras = "extrasKey";
+}
